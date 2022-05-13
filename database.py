60d37f18bb3e49baaccbee840e934d6cb095fd42
Leon Dorfling <leond@pepkorit.com>
2022-05-13 17:28:05 +0200

    Added ToDo project

---
+++ database.py
@@ -0,0 +1,14 @@
+from sqlalchemy import *
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+
+SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:Pa$$12345!@127.0.0.1:3306/todoapp"
+
+engine = create_engine (
+    SQLALCHEMY_DATABASE_URL
+)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
